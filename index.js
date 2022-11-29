dab437c2e9267a4c9f0579c22ff5cad2bb2410d9
Samuel <samuel.muthaiga@student.moringaschool.com>
2022-11-30 01:42:39 +0300

    Added comments to the index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Write your code here!
+//Removing the main element 
 const main = document.querySelector('main#main')
 main.remove()
-//Create a h1 element using 
+//Create a h1 element using using document.createElement()
 let newHeader = document.createElement("h1")
+//assigning the h1 element an id atrribute of victory
 newHeader.setAttribute("id","victory")
+//inserting a message to the h1 element using .innerHTML
 newHeader.innerHTML = `<h1>Muthaiga is the champion</h1>`
