d7f25d24447e6aa27717a9aca0ea30fe433cfbb5
Samuel <samuel.muthaiga@student.moringaschool.com>
2022-11-30 01:19:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.querySelector('main#main')
+main.remove()
+//Create a h1 element using 
+let newHeader = document.createElement("h1")
+newHeader.setAttribute("id","victory")
+newHeader.innerHTML = `<h1>Muthaiga is the champion</h1>`
